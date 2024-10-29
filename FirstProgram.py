45ac7f6e5357aa8d6006fce06e3d0a64bf862b4c
Ashraf-DIU <ashrafbinalam17@gmail.com>
2024-10-30 00:51:42 +0600

    Adding data type of python

---
+++ FirstProgram.py
@@ -0,0 +1,26 @@
+# print("Hello World")
+# print("I am Ashraf Bin Alam")
+# print("My age is 21")
+# print("My age is 21. ", "Ek line e print korte cai")
+
+# name = "Ashraf Bin Alam"
+# age = 23
+# cgpa = 3.43
+
+# print(name)
+# print(age)
+# print("My CGPA is:",cgpa)
+
+# print(type(name))
+# print(type(age))
+# print(type(cgpa))
+
+age = 23
+old = False
+a = None
+
+print(type(age))
+print(type(old))
+print(type(a))
+
+

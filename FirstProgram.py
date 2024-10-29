37680aa447f18a6413178b9338bba9382e50034d
Ashraf-DIU <ashrafbinalam17@gmail.com>
2024-10-30 01:32:54 +0600

    Arithmetic Operation in Python

--- FirstProgram.py
+++ FirstProgram.py
@@ -6,7 +6,6 @@
 # name = "Ashraf Bin Alam"
 # age = 23
 # cgpa = 3.43
-
 # print(name)
 # print(age)
 # print("My CGPA is:",cgpa)
@@ -15,12 +14,56 @@
 # print(type(age))
 # print(type(cgpa))
 
-age = 23
-old = False
-a = None
+# age = 23
+# old = False
+# a = None
+# print(type(age))
+# print(type(old))
+# print(type(a))
 
-print(type(age))
-print(type(old))
-print(type(a))
+# Arithmetic Operation in Python
 
+a = 5
+b = 3
+# sum = a+b
+# print("The Sum is:",sum)
+# diff = b-a
+# print("The Difference is:" ,diff)
+# print(a + b)
+# print(a - b)
+# print(a * b)
+# print(a / b) # Division answer always in floating
+# print(a % b) # Reminder
+# print(a ** b) # a^b
 
+# Relational Operator (# Answer in True/False)
+
+# print(a == b) 
+# print(a != b) 
+# print(a >= b)
+# print(a <= b)
+# print(a >= b)
+# print(a > b)
+
+# Assignment Operator in Python
+
+# num = 10
+# num = num + 10 #num = 20
+# num += 20 #num = 40
+# num -= 10 #num = 30
+# num *= 2 #num = 60
+# num /= 4 #num = 15
+# num %= 6 #num = 3
+# num **= 3 #num = 27
+# print("Number :", num)
+
+# Logical Operator
+
+print(not False)
+print(not True)
+print(not (a>b))
+val1 = True
+val2 = False
+print("And Operator:" ,val1 and val2) #If both True
+print("Or Operator:" ,val1 or val2) #Any one True
+print("Or Operator:", (a == b) or (a > b))

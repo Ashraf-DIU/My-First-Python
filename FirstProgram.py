88f083181f1f6a55ebfff4c212a465a5dd2de3b2
Ashraf-DIU <ashrafbinalam17@gmail.com>
2024-10-30 01:59:30 +0600

    final Commit of FirstProgram

--- FirstProgram.py
+++ FirstProgram.py
@@ -1,91 +1,91 @@
-# print("Hello World")
-# print("I am Ashraf Bin Alam")
-# print("My age is 21")
-# print("My age is 21. ", "Ek line e print korte cai")
+print("Hello World")
+print("I am Ashraf Bin Alam")
+print("My age is 21")
+print("My age is 21. ", "Ek line e print korte cai")
 
-# name = "Ashraf Bin Alam"
-# age = 23
-# cgpa = 3.43
-# print(name)
-# print(age)
-# print("My CGPA is:",cgpa)
+name = "Ashraf Bin Alam"
+age = 23
+cgpa = 3.43
+print(name)
+print(age)
+print("My CGPA is:",cgpa)
 
-# print(type(name))
-# print(type(age))
-# print(type(cgpa))
+print(type(name))
+print(type(age))
+print(type(cgpa))
 
-# age = 23
-# old = False
-# a = None
-# print(type(age))
-# print(type(old))
-# print(type(a))
+age = 23
+old = False
+a = None
+print(type(age))
+print(type(old))
+print(type(a))
 
 # Arithmetic Operation in Python
 
-# a = 5
-# b = 3
-# sum = a+b
-# print("The Sum is:",sum)
-# diff = b-a
-# print("The Difference is:" ,diff)
-# print(a + b)
-# print(a - b)
-# print(a * b)
-# print(a / b) # Division answer always in floating
-# print(a % b) # Reminder
-# print(a ** b) # a^b
+a = 5
+b = 3
+sum = a+b
+print("The Sum is:",sum)
+diff = b-a
+print("The Difference is:" ,diff)
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b) # Division answer always in floating
+print(a % b) # Reminder
+print(a ** b) # a^b
 
 # Relational Operator (# Answer in True/False)
 
-# print(a == b) 
-# print(a != b) 
-# print(a >= b)
-# print(a <= b)
-# print(a >= b)
-# print(a > b)
+print(a == b) 
+print(a != b) 
+print(a >= b)
+print(a <= b)
+print(a >= b)
+print(a > b)
 
 # Assignment Operator in Python
 
-# num = 10
-# num = num + 10 #num = 20
-# num += 20 #num = 40
-# num -= 10 #num = 30
-# num *= 2 #num = 60
-# num /= 4 #num = 15
-# num %= 6 #num = 3
-# num **= 3 #num = 27
-# print("Number :", num)
+num = 10
+num = num + 10 #num = 20
+num += 20 #num = 40
+num -= 10 #num = 30
+num *= 2 #num = 60
+num /= 4 #num = 15
+num %= 6 #num = 3
+num **= 3 #num = 27
+print("Number :", num)
 
 # Logical Operator
 
-# print(not False)
-# print(not True)
-# print(not (a>b))
-# val1 = True
-# val2 = False
-# print("And Operator:" ,val1 and val2) #If both True
-# print("Or Operator:" ,val1 or val2) #Any one True
-# print("Or Operator:", (a == b) or (a > b))
+print(not False)
+print(not True)
+print(not (a>b))
+val1 = True
+val2 = False
+print("And Operator:" ,val1 and val2) #If both True
+print("Or Operator:" ,val1 or val2) #Any one True
+print("Or Operator:", (a == b) or (a > b))
 
+# Type Conversation
 
-#Type Conversation
-
-# a = 2
-# b = 4.5
-# b = str(b)
-# # sum = a+b
-# # print(sum) #Sum = 6.5
-# print(type(b))
+a = 2
+b = 4.5
+b = str(b)
+# sum = a+b
+# print(sum) #Sum = 6.5
+print(type(b))
 
 
 # Input In python (Python normally take input in String)
-# name =  input("Enter Your Name: ")
-# print("Ok, I am ", name)
-# age =  input("Enter Your age: ")
-# print("Ok, my age is", age)
+name =  input("Enter Your Name: ")
+print("Ok, I am ", name)
+age =  input("Enter Your age: ")
+print("Ok, my age is", age)
 val = input("Enter Anything ")
 print("Checking Type:", type(val), val)
+
 #taking int input
 val2 = int(input("Taking Integer  Input: "))
 print(type(val2),val2)
